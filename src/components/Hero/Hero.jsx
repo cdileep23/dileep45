65426697d1d0ac587fcf3d8495137fed4ad25408
dileep <cdileepkumar22@gmail.com>
2025-02-02 22:36:59 +0530

    porfolio

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+import styles from "./Hero.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Hero = () => {
+  return (
+    <section className={styles.container}>
+      <div className={styles.content}>
+        <h1 className={styles.title}>Hi, I'm Dileep</h1>
+        <p className={styles.description}>
+          I'm a Full-stack developer with expertise in the MERN stack, web development, and data-driven applications.
+        </p>
+        <a href="mailto:myemail@email.com" className={styles.contactBtn}>
+          Contact Me
+        </a>
+      </div>
+      <img
+        src={getImageUrl("hero/profile.png")}
+        alt="Hero image of me"
+        className={styles.heroImg}
+      />
+      <div className={styles.topBlur} />
+      <div className={styles.bottomBlur} />
+    </section>
+  );
+};
