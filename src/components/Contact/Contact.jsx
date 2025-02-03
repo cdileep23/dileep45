fd20f1fb124e648b1470db74232d8748cc20495b
dileep <cdileepkumar22@gmail.com>
2025-02-03 08:36:53 +0530

    urls

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -20,11 +20,11 @@ export const Contact = () => {
             src={getImageUrl("contact/linkedinIcon.png")}
             alt="LinkedIn icon"
           />
-          <a href="https://www.linkedin.com/myname">https://www.linkedin.com/in/dileepkumarc003/</a>
+          <a href="https://www.linkedin.com/in/dileepkumarc003/">https://www.linkedin.com/in/dileepkumarc003/</a>
         </li>
         <li className={styles.link}>
           <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
-          <a href="https://www.github.com/myname">https://github.com/cdileep23</a>
+          <a href="https://github.com/cdileep23">https://github.com/cdileep23</a>
         </li>
       </ul>
     </footer>
